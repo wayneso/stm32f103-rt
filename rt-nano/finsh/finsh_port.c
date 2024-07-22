d28505b4b28846872627746e4a00f2012e435943
waynesy <2546850503@qq.com>
2024-07-22 10:13:36 +0800

    使用MXCube生成基于stm32f103c8t6的MDK工程，植入rtthread-nano，finsh组件

---
+++ rt-nano/finsh/finsh_port.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ */
+
+#include <rthw.h>
+#include <rtconfig.h>
+#include <rtthread.h>
+
+#ifndef RT_USING_FINSH
+#error Please uncomment the line <#include "finsh_config.h"> in the rtconfig.h 
+#endif
+
+#ifdef RT_USING_FINSH
+
+static void reboot(rt_uint8_t argc, char **argv)
+{
+    rt_hw_cpu_reset();
+}
+FINSH_FUNCTION_EXPORT_ALIAS(reboot, __cmd_reboot, Reboot System);
+
+#endif /* RT_USING_FINSH */
+
